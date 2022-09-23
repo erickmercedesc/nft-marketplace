773fd674af20b9574d22eae8db87b6106da5778f
Erick Mercedes <erickmercedesemc@gmail.com>
2022-09-23 02:57:34 -0400

    fix bug

--- app/Http/Controllers/DepositController.php
+++ app/Http/Controllers/DepositController.php
@@ -23,17 +23,17 @@ class DepositController extends Controller
         ]);
     }
 
-    public function address(Request $request, $currency)
+    public function address(Request $request, $_currency)
     {
-        $currency = str_replace('-', '/', $currency);
+        $currency = str_replace('-', '/', $_currency);
 
         $ca = Http::get("https://api.blockbee.io/$currency/create/", [
             'apikey' => env('CRYPTAPI_TOKEN'),
-            'callback' => route('webhook.cryptapi', ['user' => $request->user(), 'currency' => $currency])
+            'callback' => route('webhook.cryptapi', ['user' => $request->user(), 'currency' => $_currency])
         ] )->json(); 
 
         Log::debug(json_encode($ca));
-        
+
         return response()->json([
             'address' => $ca['address_in']
         ]);
