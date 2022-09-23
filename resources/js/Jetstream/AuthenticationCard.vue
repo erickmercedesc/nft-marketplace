49aa9de32acb0445411f239de03ee940567c9ee7
Erick Mercedes <erickmercedesemc@gmail.com>
2022-09-22 20:36:10 -0400

    fix bug

--- resources/js/Jetstream/AuthenticationCard.vue
+++ resources/js/Jetstream/AuthenticationCard.vue
@@ -2,10 +2,10 @@
     <div id="background" class="min-h-screen flex flex-col sm:justify-center items-center pt-2 sm:pt-0">
         <div class="bg-white grid grid-cols-2 w-full sm:max-w-6xl rounded">
             <div class="hidden md:grid grid-cols-2">
-                <img class="h-full" src="/img/nft_1.png" alt="">
-                <img class="h-full" src="/img/nft_2.png" alt="">
-                <img class="h-full" src="/img/nft_3.png" alt="">
-                <img class="h-full" src="/img/nft_4.png" alt="">
+                <img class="h-full" src="/img/nft1.png" alt="">
+                <img class="h-full" src="/img/nft2.png" alt="">
+                <img class="h-full" src="/img/nft3.png" alt="">
+                <img class="h-full" src="/img/nft4.png" alt="">
             </div>
 
             <div class="col-span-2 md:col-span-1 mt-12 md:mt-0 flex flex-col justify-center text-center px-4 py-2">
